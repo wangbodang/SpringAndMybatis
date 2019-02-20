bbab951c8cdb0614d57ea11db8bb4f5923c46f57
wangbodang <870719738@qq.com>
2019-02-20 17:57:46 +0800

    添加log4j2日志功能

--- src/test/java/spring/ContextTest.java
+++ src/test/java/spring/ContextTest.java
@@ -4,18 +4,21 @@ import com.wangbodang.common.spring.BeanEntity;
 import com.wangbodang.demo.entity.DemoEntity;
 import com.wangbodang.demo.entity.Employee;
 import com.wangbodang.demo.service.impl.Apple;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class ContextTest {
-
+    private static Logger logger = LogManager.getLogger(ContextTest.class.getName());
     /**
      * 测试spring容器
      */
     @Test
     public void contextTest01(){
+        logger.info("\n===> 测试log4j2");
         String config = "conf/spring-conf.xml";
         ApplicationContext ac = new ClassPathXmlApplicationContext(config);
 
